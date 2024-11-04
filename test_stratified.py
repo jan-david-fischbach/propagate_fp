85c12d5879006fb8d2da7539745bdc72c41284a2
Jan David Fischbach <mail@jan-dav.id>
2024-11-04 13:43:14 +0100

    Some memory leak testing: Doesn't occur with newest `jax` (0.4.35)

--- test_stratified.py
+++ test_stratified.py
@@ -5,14 +5,18 @@ import angled_stratified
 import angled_stratified_treams
 import fresnel
 import sax
+import jax
+import psutil
+from diffaaable import aaa
+import time
+
+# from jax_smi import initialise_tracking
+# initialise_tracking()
 #%%
 if __name__ == "__main__":
     ns = [2, 1.889+0.0035j, 1.802+0.0562j, 2.321+0.291j, 1.519+9j]
     # ns = [2, 1.889, 1.802, 2.321, 9j]
     ds = [1, 0.15, 0.03, 0.3, 0.2]
-
-    # ns = [4, 2]
-    # ds = [0.9, 1]
 
     wl = 1.5
     theta_0=0.7#0.99 * jnp.pi/2
@@ -31,14 +35,15 @@ if __name__ == "__main__":
     )
     smat = stack()
 
+    stack, info = angled_stratified.stack_smat_kx(ds, ns, k0, kx, pol=pol)
+    smat_kx = stack()
+
     stack, info = angled_stratified.stack_smat_kx(
         ds,
           ns, k0, kx, pol=pol, fresnel=fresnel.fresnel_kx_direct
     )
+    del info
     smat_kx_direct = stack()
-
-    stack, info = angled_stratified.stack_smat_kx(ds, ns, k0, kx, pol=pol)
-    smat_kx = stack()
 
     S          , portmap = sax.sdense(smat)
     S_kx       , portmap = sax.sdense(smat_kx)
@@ -55,31 +60,18 @@ if __name__ == "__main__":
     k_i = jnp.linspace(-0.2*k0, 0.2*k0, 301)
     K_r, K_i = jnp.meshgrid(k_r, k_i)
 
-    kx = jnp.linspace(0.1*k0, 0.7*k0, 7)
+    kx = jnp.linspace(0.1*k0, 0.7*k0, 200)
     k0_mesh = K_r+ 1j*K_i
 
     k0_mesh = k0_mesh[None, :]
     kx = kx[:, None, None]
-
 #%%
-    smat_mesh = stack(
-        k0=k0_mesh, kx=kx, 
-        bc_angle=0.7*jnp.pi, bc_arcsin_i=0.7*jnp.pi, bc_arcsin_j=0.7*jnp.pi)
-
-    from diffaaable import aaa
-    for kx_idx in range(len(kx)):
-        plt.figure()
-        downsample = 20
-        z_j, f_j, w_j, z_n = aaa(
-            k0_mesh[0, ::downsample, ::downsample], 
-            smat_mesh[('in', 'out')][kx_idx][::downsample, ::downsample], 
-            tol=1e-7
-        )
-
-        plt.pcolormesh(K_r, K_i, jnp.abs(smat_mesh[('in', 'in')][kx_idx]), norm="log", vmin=0.1)
-        plt.xlim(plt.xlim())
-        plt.ylim(plt.ylim())
-        plt.scatter(z_n.real, z_n.imag)
-        plt.show()
-
-# %%
+    stack = jax.jit(stack)
+#%%
+    for i in range(5):
+        smat_mesh = stack(
+            k0=k0_mesh, kx=kx, 
+            bc_angle=0.7*jnp.pi, bc_arcsin_i=0.7*jnp.pi, bc_arcsin_j=0.7*jnp.pi)
+        jax.block_until_ready(smat_mesh)
+        time.sleep(1)
+        print(f"Memory {i}:",psutil.Process().memory_info().rss / (1024 * 1024))
