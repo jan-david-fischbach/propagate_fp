e946821a3cf790136b0bab4aa246a7e005230666
Jan David Fischbach <mail@jan-dav.id>
2024-11-05 11:36:45 +0100

    clean up: only keep direct eval

--- fresnel.py
+++ fresnel.py
@@ -1,69 +1,5 @@
-#%%
 import jax.numpy as jnp
-from arcsin import arcsin
 from propagation import angled_sqrt
-from pprint import pprint
-import sax
-
-def debug_sdict(sdict):
-    S, portmap = sax.sdense(sdict)
-    #pprint(S)
-
-def fresnel_mirror_ij(ni=1, nj=1, theta_0=0, pol="s", bc_arcsin_i=jnp.pi, bc_arcsin_j=jnp.pi, **kwargs):
-    """Model a (fresnel) interface between two refractive indices
-
-    Args:
-        ni: refractive index of the initial medium
-        nj: refractive index of the final
-        theta: angle of incidence measured from normal in vacuum
-        pol: "s" or "p" polarization
-    """
-
-    theta_i = arcsin(jnp.sin(theta_0)/ni, bc_arcsin=bc_arcsin_i)
-    theta_j = arcsin(jnp.sin(theta_0)/nj, bc_arcsin=bc_arcsin_j) #need to investigate
-
-    # theta_i = jnp.arcsin(jnp.sin(theta_0)/ni)
-    # theta_j = jnp.arcsin(jnp.sin(theta_0)/nj)
-
-    cos_i = jnp.cos(theta_i)
-    cos_j = jnp.cos(theta_j)
-
-    if pol in ["s", "TE"]:
-        r_ij = (ni*cos_i-nj*cos_j) / (ni*cos_i + nj*cos_j)  
-        # i->i reflection
-        t_ij = 2*ni*cos_i / (ni*cos_i + nj*cos_j)  # i->j transmission
-        t_ji = 2*nj*cos_j / (ni*cos_i + nj*cos_j) 
-        
-    elif pol in ["p", "TM"]:
-        r_ij = (nj*cos_i-ni*cos_j) / (nj*cos_i + ni*cos_j)  
-        # i->i reflection
-        t_ij = 2*ni*cos_i / (nj*cos_i + ni*cos_j)  # i->j transmission
-        t_ji = 2*nj*cos_j / (nj*cos_i + ni*cos_j)
-
-    else:
-        raise ValueError(f"polarization should be either 's'/'TM' or 'p'/'TE'")
-    
-    r_ji = -r_ij  # j -> i reflection
-    
-    sdict = {
-        ("left", "left"): r_ij,
-        ("left", "right"): t_ij,
-        ("right", "left"): t_ji,
-        ("right", "right"): r_ji,
-    }
-    # print(f"{ni=}, {nj=}:")
-    debug_sdict(sdict)
-    return sdict
-
-def fresnel_kx(
-        ni=1, nj=1, k0=1, kx=0, pol="s", 
-        bc_arcsin=jnp.pi, bc_arcsin_i=jnp.pi, bc_arcsin_j=jnp.pi,
-        **kwargs):
-    theta_0 = arcsin(kx/k0 + 0j, bc_arcsin=bc_arcsin)
-    return fresnel_mirror_ij(
-        ni=ni, nj=nj, theta_0=theta_0, pol=pol, 
-        bc_arcsin=bc_arcsin, bc_arcsin_i=bc_arcsin_i, bc_arcsin_j=bc_arcsin_j,
-        **kwargs)
 
 def fresnel_kx_direct(
     ni=1, nj=1, k0=1, kx=0, pol="p", bc_angle=jnp.pi, **kwargs
@@ -84,7 +20,7 @@ def fresnel_kx_direct(
     t_ij = 2*kiz / (eta*kiz-kjz/eta)
     t_ji = (1-r_ij**2)/t_ij
 
-    r_ji = -r_ij  # j -> i reflection
+    r_ji = -r_ij
 
     sdict = {
         ("left", "left"): r_ij,
@@ -92,20 +28,5 @@ def fresnel_kx_direct(
         ("right", "left"): t_ji,
         ("right", "right"): r_ji,
     }
-    # print(f"{ni=}, {nj=}:")
-    debug_sdict(sdict)
-    return sdict
 
-#%%
-if __name__ == "__main__":
-    wl = 1.5
-    k0 = 2*jnp.pi/wl
-    theta_0=0.7
-    kx = k0*jnp.sin(theta_0)
-    pol = "s"
-    n1 = 4
-# %%
-    fresnel_kx_direct(nj=n1, k0=k0, kx=kx, pol=pol)
-# %%
-    fresnel_mirror_ij(nj=n1, theta_0=theta_0, pol=pol)
-# %%
+    return sdict
--- test_stratified.py
+++ test_stratified.py
@@ -1,25 +1,14 @@
-# %%
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
 import angled_stratified
 import angled_stratified_treams
-import fresnel
 import sax
-import jax
-import psutil
-from diffaaable import aaa
-import time
 
-# from jax_smi import initialise_tracking
-# initialise_tracking()
-#%%
 if __name__ == "__main__":
     ns = [2, 1.889+0.0035j, 1.802+0.0562j, 2.321+0.291j, 1.519+9j]
-    # ns = [2, 1.889, 1.802, 2.321, 9j]
     ds = [1, 0.15, 0.03, 0.3, 0.2]
 
     wl = 1.5
-    theta_0=0.7#0.99 * jnp.pi/2
+    theta_0=0.7
     k0 = 2*jnp.pi/wl
     kx = k0*jnp.sin(theta_0)
 
@@ -30,48 +19,12 @@ if __name__ == "__main__":
         ds, ns, k0=k0, kx=kx, poltype="parity"
     )
 
-    stack, info = angled_stratified.stack_smat(
-        ds, ns, wl, theta_0=theta_0, pol=pol
-    )
-    smat = stack()
-
-    stack, info = angled_stratified.stack_smat_kx(ds, ns, k0, kx, pol=pol)
-    smat_kx = stack()
-
     stack, info = angled_stratified.stack_smat_kx(
-        ds,
-          ns, k0, kx, pol=pol, fresnel=fresnel.fresnel_kx_direct
+        ds, ns, k0, kx, pol=pol
     )
-    del info
     smat_kx_direct = stack()
 
-    S          , portmap = sax.sdense(smat)
-    S_kx       , portmap = sax.sdense(smat_kx)
     S_kx_direct, portmap = sax.sdense(smat_kx_direct)
-
     S_treams = jnp.array(smat_treams)[:,:,pol_idx, pol_idx][::-1]
 
-    assert jnp.allclose(S, S_kx_direct)                 
-    assert jnp.allclose(S_kx, S_kx_direct)
     assert jnp.allclose(S_treams, S_kx_direct)
-
-# %%
-    k_r = jnp.linspace(-0*k0, 1*k0, 300)
-    k_i = jnp.linspace(-0.2*k0, 0.2*k0, 301)
-    K_r, K_i = jnp.meshgrid(k_r, k_i)
-
-    kx = jnp.linspace(0.1*k0, 0.7*k0, 200)
-    k0_mesh = K_r+ 1j*K_i
-
-    k0_mesh = k0_mesh[None, :]
-    kx = kx[:, None, None]
-#%%
-    stack = jax.jit(stack)
-#%%
-    for i in range(5):
-        smat_mesh = stack(
-            k0=k0_mesh, kx=kx, 
-            bc_angle=0.7*jnp.pi, bc_arcsin_i=0.7*jnp.pi, bc_arcsin_j=0.7*jnp.pi)
-        jax.block_until_ready(smat_mesh)
-        time.sleep(1)
-        print(f"Memory {i}:",psutil.Process().memory_info().rss / (1024 * 1024))
